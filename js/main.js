77fde05e23d3975ced898b774a655f18f9c4723d
marcormun <mcordontrabajo@gmail.com>
2022-05-30 09:27:55 +0200

    solucion github pages imagenes 2

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ const luchar = () => {
     //Comprobamos que sigan con vida los dos y si no llamamos a la pantalla final
     if(presidente1.vida<=0){
         presidente2.ganador = true;
-        document.getElementById("pantalla4").style.backgroundImage = `url("../img/banderaRusia.jpg")`;
+        document.getElementById("pantalla4").style.backgroundImage = `url("./img/banderaRusia.jpg")`;
         document.querySelector("#pantalla4").innerHTML = PUTIN;
         let textoGanador = document.createElement("p");
         textoGanador.innerText = "Putin wins"
@@ -45,7 +45,7 @@ const luchar = () => {
         cambiarPantalla(4);
     }else if(presidente2.vida<=0){
         presidente1.ganador = true;   
-        document.getElementById("pantalla4").style.backgroundImage = `url("../img/banderaEEUU.jpg")`;
+        document.getElementById("pantalla4").style.backgroundImage = `url("./img/banderaEEUU.jpg")`;
         document.querySelector("#pantalla4").innerHTML = BIDEN;
         let textoGanador = document.createElement("p");
         textoGanador.innerText = "Biden wins"
